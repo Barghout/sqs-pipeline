5205312f7ce6cd13cdcc88280c95e31a4aebc19c
Barghout <khaled_barghout@yahoo.com>
2025-12-11 19:40:36 +0200

    Remove unwanted file

--- src/python create_queue.py
+++
@@ -1,8 +0,0 @@
-# create_queue.py
-from localstack_client.session import Session
-
-session = Session()
-sqs = session.client("sqs", endpoint_url="http://localhost:4566")
-
-queue = sqs.create_queue(QueueName="my-queue")
-print("Queue created:", queue['QueueUrl'])
